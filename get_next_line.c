1b965d12b3c4587a52851c5ca4697ba17cf2f6bc
Danae Duvivier <dduvivie@fu-r4-p7.s19.be>
2021-11-24 16:48:37 +0100

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -10,4 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
+char	*get_next_line(int fd)
+{
+	char	*buff;
+	int		rc;
+
+	buff = malloc(BUFFER_SIZE + 1 * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rc = read(fd, buff, BUFFER_SIZE);
+	if (rc == -1)
+	{
+		printf("file read error\n");
+		return (NULL);
+	}
+	else
+	{
+		buff[rc] = '\0';
+		return (buff);
+	}
+
+}
+
+int main(void)
+{
+	int	fd;
+	char *s;
+
+	fd = open("text1.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("file open error\n");
+		return(0);
+	}
+	s = get_next_line(fd);
+	printf("%s\n", s);
+	close(fd);
+	return (0);
+}
